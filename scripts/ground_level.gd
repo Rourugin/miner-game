c4bf0ac233c288a5952614780fe342c31c9bc7c8
Rourugin <maks.4min@gmail.com>
2026-07-04 17:01:40 +0200

    update player and level sprites

---
+++ scripts/ground_level.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_home_area_body_entered(_body: Node2D) -> void:
+	print("Home entered")
+
+
+func _on_home_area_2_body_entered(_body: Node2D) -> void:
+	print("mine entered")
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-const SPEED = 300.0
+
+const SPEED = 100.0
 
 var last_direction: Vector2
 
@@ -14,12 +15,18 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _process_movement(direction: Vector2) -> void:
-	if direction != Vector2.ZERO:
-		velocity = direction * SPEED
-		last_direction = direction
-		_play_animation("run")
-	elif direction == Vector2.ZERO:
-		_play_animation("idle")
+	match direction:
+		Vector2.ZERO:
+			velocity = Vector2.ZERO
+			_play_animation("idle")
+		Vector2.RIGHT, Vector2.LEFT:
+			velocity = direction * SPEED
+			last_direction = direction
+			_play_animation("run")
+		Vector2.UP, Vector2.DOWN:
+			if get_tree().current_scene.name == "ground_level":
+				velocity = Vector2.ZERO
+				_play_animation("idle")
 		
 func _play_animation(anim: String) -> void:
 	if anim == "idle":
@@ -28,7 +35,7 @@ func _play_animation(anim: String) -> void:
 		animated_sprite_2d.play("run")
 		if last_direction == Vector2.RIGHT:
 			animated_sprite_2d.flip_h = false
-			collision_polygon_2d.position.x = abs(collision_polygon_2d.position.x)
+			collision_shape_2d.position.x = abs(collision_shape_2d.position.x)
 		elif last_direction == Vector2.LEFT:
 			animated_sprite_2d.flip_h = true
-			collision_polygon_2d.position.x = abs(collision_polygon_2d.position.x) * -1
+			collision_shape_2d.position.x = abs(collision_shape_2d.position.x) * -1
